a69e83887d634e2f31203d9caf484b1b886d87f0
Lucas Rodrigues Monteiro <180125974@aluno.unb.br>
2025-06-26 23:15:19 -0300

    fontes

--- main.c
+++ main.c
@@ -13,6 +13,9 @@ int main(){
     TTF_Init();
 
     SDL_Window *window = SDL_CreateWindow("Arkanoid ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_HEIGHT, SCREN_WIDTH,  0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    TTF_Font *font = TTF_OpenFont("fonts/Sony_Sketch_Bold_205.ttf", 20);
+
     bool running = true;
     int counter = 1000;
     while(running){
